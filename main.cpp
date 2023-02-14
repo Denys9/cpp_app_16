7ba1b94b7d298614fe2af49506881453f16b5958
lozin <lozindenis6@gmail.com>
2023-02-14 20:12:35 +0200

    №1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
-int** addColumn(int**& a, size_t m, size_t n, int value)
+int** addColumn(int**& a, int m, int n, int value)
 {
-    int** tmp = new int* [n + value] {0};
+    int** tmp = new int* [n + value];
     for (int i = m - 1; i >= 0; i--)
     {
         tmp[i + value] = a[i];
@@ -18,29 +19,34 @@ int** addColumn(int**& a, size_t m, size_t n, int value)
 
 int main()
 {
+    srand(time(0));
+    int randd;
     int i, j;
     int colon, line;
-    cout << "Enter colon < line ->";
+    cout << "colon < line - ";
     cin >> colon;
-    cout << "\nEnter line ->";
+    cout << "\nline - ";
     cin >> line;
-    int** array = new int* [line] {0};
+    int** array = new int* [line];
     for (i = 0; i < line; i++)
     {
-        array[i] = new int[colon] {0};
+        array[i] = new int[colon];
     }
     for (i = 0; i < colon; i++)
     {
         for (j = 0; j < line; j++)
         {
-            array[i][j] = 1;
-
+            array[i][j] = randd;
+            for (size_t j = 0; j < randd; j++)
+            {
+                array[i][j] = rand() % 10;
+            }
             cout << array[i][j] << '\t';
         }
         cout << '\n';
     }
     int add;
-    cout << "Input add: ";
+    cout << "add - ";
     cin >> add;
     array = addColumn(array, colon, line, add);
     colon += add;
@@ -48,8 +54,11 @@ int main()
     {
         for (j = 0; j < line; j++)
         {
-            array[i][j] = 1;
-
+            array[i][j] = randd;
+            for (size_t j = 0; j < randd; j++)
+            {
+                array[i][j] = rand() % 10;
+            }
             cout << array[i][j] << '\t';
         }
         cout << '\n';
